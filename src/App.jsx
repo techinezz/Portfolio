45b79390b43e1bac4fb5069070d885c8fc7eb07b
techinezz <y.martinez.tech@gmail.com>
2024-01-03 02:41:43 -0500

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter } from "react-router-dom";
 
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works } from "./components";
 
 const App = () => {
   return (
-    <div>
-      Personal Website
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Works />
+      <Tech />
+      <Feedbacks />
+
     </div>
+    </BrowserRouter>
   )
 }
 
