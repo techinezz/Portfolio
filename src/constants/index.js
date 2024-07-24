fc70e7084a8ee644f39adb1ff4445f89eeb13327
techinezz <y.martinez.tech@gmail.com>
2024-07-24 15:21:49 -0400

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -213,19 +213,6 @@ import {
         "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       ],
     },
-    {
-      title: "COMING SOON",
-      company_name: "COMPANY",
-      icon: comingsoon,
-      iconBg: "#E6DEDD",
-      date: "COMING SOON",
-      points: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      ],
-    },
   ];
   
   export {technologies, experiences, projects };
